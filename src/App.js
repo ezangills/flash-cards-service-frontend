165a83945e65f78f588f6da7bc3669f831bed4ae
Цеев Черим 29108 RU <cherim.tseev@dpd.ru>
2022-10-25 20:52:58 +0400

    added login in with real api

--- src/App.js
+++ src/App.js
@@ -4,12 +4,39 @@ import { useState } from 'react'
 import Verify from './components/Verify';
 
 const App = () => {
-  const [state, setState] = useState('openSignInPage')
+  const [state, setState] = useState({name: 'openSignInPage', message: ''})
+
+  const useOpenApp = (success, responseBody) => {
+    if (success === 200) {
+      setState({name: 'openAppPage', message: ''})
+    } else if (success === 401) {
+      setState({name: 'openSignInPage', message: 'No user matching that username and password combination'})
+    }
+  }
+
+  const signup = () => {
+    setState({name: 'openSignUpPage', message: ''})
+  }
+
+  const signin = () => {
+    setState({name: 'openSignInPage', message: ''})
+  }
+
+  const verify = () => {
+    setState({name: 'openVerifyPage', message: ''})
+    /* Verify page will open only if registration was successful, otherwise show error message */ 
+  }
+
+  const postVerifySignin = () => {
+    setState({name: 'openSignInPage', message: ''}) 
+    /* Open signin page only on successful verification, otherwise error message */
+  }
+
   return (
     <div className='page'>
-      {state === 'openSignInPage' && <Login openapp={() => setState('openAppPage') /* Here if we get error the state should be set to verify and ask back to send code to email again */ } signup={() => setState('openSignUpPage') } />}
-      {state === 'openSignUpPage' && <Signup signin={() => setState('openSignInPage') } verify={() => setState('openVerifyPage') /* Verify page will open only if registration was successful, otherwise show error message */ } />}
-      {state === 'openVerifyPage' && <Verify enter={() => setState('openSignInPage') /* Open signin page only on successful verification, otherwise error message */ } />}
+      {state.name === 'openSignInPage' && <Login errorMessage={state.message} openapp={useOpenApp} signup={signup} />}
+      {state.name === 'openSignUpPage' && <Signup signin={signin} verify={verify} />}
+      {state.name === 'openVerifyPage' && <Verify enter={postVerifySignin} />}
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,43 @@
 import { Button, Checkbox, FormControlLabel, Grid, Link, Paper, TextField, Typography } from "@material-ui/core"
-import React from "react"
+import { Alert } from '@material-ui/lab'
+import React, { useRef } from "react"
 import "./login.css" 
 
 const Login = (props) => {
     const paperStyle={height: '20em', width: '17em', padding: 20}
     const textFieldStyle={marginTop: '1em'}
     const linksStyle = {marginTop: '2em'}
+
+    const usernameRef = useRef('usernameRef')
+    const passwordRef = useRef('passwordRef')
+
+    console.log(props.errorMessage)
+
+    const openapp = async () => {
+        const response = await fetch('http://localhost:8080/api/auth/signin', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: usernameRef.current.value,
+                password: passwordRef.current.value,
+            })
+        })
+        props.openapp(response.status, await response.json());
+    }
+
     return (
         <Grid>
             <Paper elevation={10} style={paperStyle}>
-                    <TextField style={textFieldStyle} id="outlined-basic" label="Username" variant="outlined" placeholder="Username" fullWidth required />
-                    <TextField style={textFieldStyle} id="outlined-basic" type="password" label="Password" variant="outlined" placeholder="Password" fullWidth required />
+                    {props.errorMessage !== '' &&
+                        <Alert severity="error">{props.errorMessage}</Alert>
+                    }
+                    <TextField style={textFieldStyle} inputRef={usernameRef} id="username" label="Username" variant="outlined" placeholder="Username" fullWidth required />
+                    <TextField style={textFieldStyle} inputRef={passwordRef} id="password" type="password" label="Password" variant="outlined" placeholder="Password" fullWidth required />
                     <FormControlLabel control={<Checkbox name="remember-me" control="primary" />} label="Remember me" />
-                    <Button onClick={props.openapp} variant="contained" type="submit" color="primary" fullWidth>Sign in</Button>
+                    <Button onClick={openapp} variant="contained" type="submit" color="primary" fullWidth>Sign in</Button>
                     <Grid style={linksStyle} container direction="row" justifyContent="space-between" alignItems="center">
                         <Typography display="inline">
                         <Link href='#'>Forgot password</Link>
